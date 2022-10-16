787e5c10b62781bd474daa9d44b68a058a6da357
SycoticSociety <108892965+SycoticSociety@users.noreply.github.com>
2022-10-16 03:28:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ setInterval(logoOpacity, 5);
 //MetaMask wallet connect Test
 
 
-document.getElementById('connectButton', connect);
+document.getElementById('** Connect Button **', connect);
 
 function connect() {
   ethereum
@@ -56,24 +56,3 @@ function connect() {
       }
     });
 }
-
-
-
-
-
-
-document getElementByid ('Meta Connect Soon').add
-     Event Listener ('click',event=>{let account;
-     let button= event.target;
-ethereum request ({method: 'eth_request accounts'
-     }).then(accounts=>{account=accounts[0];
-     console.log(account); button.text content=
-     account;
-ethereum request ({method: 'eth_get balance',Paramus
-     :[account,'latest]}).then(result =>{console
-     _log(result); let wei= pareint(result,16);
-     let balance= wei/(10**18); console.log
-     (balance+ "eth");
-     });
-     });
-     });
