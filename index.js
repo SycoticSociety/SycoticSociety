47b6f38e61bdfbd67db399b8be1e9bcfaca1dc3d
SycoticSociety <108892965+SycoticSociety@users.noreply.github.com>
2022-10-16 04:18:28 -0400

    Update index.js

--- index.js
+++ index.js
@@ -56,3 +56,18 @@ function connect() {
       }
     });
 }
+
+
+import detectEthereumProvider from '@metamask/detect-provider';
+
+const provider = await detectEthereumProvider();
+
+if (provider) {
+  // From now on, this should always be true:
+  // provider === window.ethereum
+  startApp(provider); // initialize your app
+} else {
+  console.log('Please install MetaMask!');
+}
+
+
