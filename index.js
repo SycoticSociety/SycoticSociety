e240db6938afb23c98b74e476722eb16a92aa375
SycoticSociety <108892965+SycoticSociety@users.noreply.github.com>
2022-10-25 16:16:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -39,24 +39,3 @@ function logoOpacity() {
 setInterval(logoOpacity, 5);
 
 
-
-/*********************************************/
-/* Access the user's accounts (per EIP-1102) */
-/*********************************************/
-document.getElementById('connectButton', connect).addEventListener('click', event => {
-  let account;
-  
-function connect() {
-  ethereum
-    .request({ method: 'eth_requestAccounts' })
-    .then(handleAccountsChanged)
-    .catch((err) => {
-      if (err.code === 4001) {
-        // EIP-1193 userRejectedRequest error
-        // If this happens, the user rejected the connection request.
-        console.log('Please connect to MetaMask.');
-      } else {
-        console.error(err);
-      }
-    });
-}
