a01aaf6306125957c80f1530256140e3ec3c698d
Sycotic Society <thecrazydutchman2000@gmail.com>
2022-12-05 13:54:17 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,7 @@ const mainMenu = document.querySelector('.mainMenu');
 const closeMenu = document.querySelector('.closeMenu');
 const openMenu = document.querySelector('.openMenu');
 const menu_items = document.querySelectorAll('nav .mainMenu li a');
-const fafabars = document.querySelector('fa fa-bars');
 
-
-fa fa-bars.addEventListener('click',show);
 openMenu.addEventListener('click',show);
 closeMenu.addEventListener('click',close);
 
