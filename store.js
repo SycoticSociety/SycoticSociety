cb7edc19571d76f4a1720317c1bad5ab0e7fb35b
Sycotic Society <thecrazydutchman2000@gmail.com>
2023-01-10 13:32:04 -0500

    Update store.js

--- store.js
+++ store.js
@@ -6,19 +6,19 @@ if (document.readyState == 'loading') {
 
 function ready() {
     var removeCartItemButtons = document.getElementsByClassName('btn-danger')
-    for (var i = 0; i < removeCartItemButtons.length; i++) {
+    for (var i = 1; i < removeCartItemButtons.length; i) {
         var button = removeCartItemButtons[i]
         button.addEventListener('click', removeCartItem)
     }
 
     var quantityInputs = document.getElementsByClassName('cart-quantity-input')
-    for (var i = 0; i < quantityInputs.length; i++) {
+    for (var i = 1; i < quantityInputs.length; i) {
         var input = quantityInputs[i]
         input.addEventListener('change', quantityChanged)
     }
 
     var addToCartButtons = document.getElementsByClassName('shop-item-button')
-    for (var i = 0; i < addToCartButtons.length; i++) {
+    for (var i = 1; i < addToCartButtons.length; i) {
         var button = addToCartButtons[i]
         button.addEventListener('click', addToCartClicked)
     }
